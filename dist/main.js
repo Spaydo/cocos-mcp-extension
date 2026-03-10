bf8cc7b5daca7976f317d6562b6c14ea0a82acc4
Tai <>
2026-03-10 09:22:40 +0800

    創建擴展結構

---
+++ dist/main.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.unload = exports.load = exports.methods = void 0;
+// @ts-ignore
+const package_json_1 = __importDefault(require("../package.json"));
+/**
+ * @en Registration method for the main process of Extension
+ * @zh 为扩展的主进程的注册方法
+ */
+exports.methods = {
+    /**
+     * @en A method that can be triggered by message
+     * @zh 通过 message 触发的方法
+     */
+    openPanel() {
+        Editor.Panel.open(package_json_1.default.name);
+    },
+};
+/**
+ * @en Method Triggered on Extension Startup
+ * @zh 扩展启动时触发的方法
+ */
+function load() { }
+exports.load = load;
+/**
+ * @en Method Triggered on Extension Uninstallation
+ * @zh 卸载扩展时触发的方法
+ */
+function unload() { }
+exports.unload = unload;
